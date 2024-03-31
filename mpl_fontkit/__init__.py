da2cb268995652facddc65c3b21f46f74370654f
Mr-Milk <yzheng@cemm.at>
2024-03-31 14:42:17 +0200

    Change build backend and adopt new PYPI authentication in CI

--- mpl_fontkit/__init__.py
+++ mpl_fontkit/__init__.py
@@ -1,2 +1,5 @@
+"""Easy font management for Matplotlib."""
+__version__ = "0.5.0"
+
 from .core import install, install_fontawesome, set_font, current_font, \
     list_fonts, show, show_fonts, font_table, font_entries
