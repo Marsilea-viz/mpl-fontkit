5ec4d5debea1b1b783c4c4308637fb26da16c575
Mr-Milk <yzheng@cemm.at>
2024-03-31 15:11:39 +0200

    Add deps

--- mpl_fontkit/__init__.py
+++ mpl_fontkit/__init__.py
@@ -1,5 +1,5 @@
 """Easy font management for Matplotlib."""
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 from .core import install, install_fontawesome, set_font, current_font, \
     list_fonts, show, show_fonts, font_table, font_entries
